dd1d4395bc42352a20d3a6ac33be31592e9e063f
seccad yeşilkaya <seccad.yesilkaya@esarj.com>
2025-02-28 01:30:37 +0300

    Added settings icon that include two icon

--- lib/view/screens/zikir/zikir_screen.dart
+++ lib/view/screens/zikir/zikir_screen.dart
@@ -8,13 +8,16 @@ import '../../../constant/constants.dart';
 String imageName = 'zikir.jpg';
 
 class ZikirScreen extends StatefulWidget {
+  const ZikirScreen({super.key});
+
   @override
-  _ZikirScreenState createState() => _ZikirScreenState();
+  ZikirScreenState createState() => ZikirScreenState();
 }
 
-class _ZikirScreenState extends State<ZikirScreen> {
+class ZikirScreenState extends State<ZikirScreen> {
   int counter = 0;
   bool isVibrationEnabled = false;
+  bool _showIcons = false;
 
   @override
   void initState() {
@@ -78,38 +81,75 @@ class _ZikirScreenState extends State<ZikirScreen> {
                 ),
               ),
             ),
-            Positioned.fill(
+            Positioned(
               top: MediaQuery.of(context).size.height / 15,
               right: MediaQuery.of(context).size.width / 15,
-              child: Container(
-                height: 25,
-                width: 55,
-                alignment: Alignment.topRight,
-                child: InkWell(
-                  child: Icon(Icons.autorenew,
-                      size: 32,
-                      color: imageName == 'zikir.jpg' ? ColorStyles.appBackGroundColor : ColorStyles.appTextColor),
-                  onTap: () => _resetCounter(),
-                ),
-              ),
-            ),
-            Positioned(
-              top: MediaQuery.of(context).size.height / 8,
-              right: MediaQuery.of(context).size.width / 15,
-              child: Container(
-                height: 25,
-                width: 55,
-                alignment: Alignment.topRight,
-                child: InkWell(
-                    child: Icon(Icons.image_outlined,
-                        size: 32,
-                        color: imageName == 'zikir.jpg' ? ColorStyles.appBackGroundColor : ColorStyles.appTextColor),
-                    onTap: () {
-                      setState(() {
-                        print('Image Changed');
-                        imageName = imageName == 'zikir.jpg' ? 'meshed.jpeg' : 'zikir.jpg';
-                      });
-                    }),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Visibility(
+                    visible: _showIcons,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          child: Container(
+                            height: 25,
+                            width: 55,
+                            alignment: Alignment.topRight,
+                            child: InkWell(
+                              child: Icon(Icons.autorenew,
+                                  size: 32,
+                                  color: imageName == 'zikir.jpg'
+                                      ? ColorStyles.appBackGroundColor
+                                      : ColorStyles.appTextColor),
+                              onTap: () => _resetCounter(),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          child: Container(
+                            height: 25,
+                            width: 55,
+                            alignment: Alignment.topRight,
+                            child: InkWell(
+                              child: Icon(Icons.dark_mode,
+                                  size: 32,
+                                  color: imageName == 'zikir.jpg'
+                                      ? ColorStyles.appBackGroundColor
+                                      : ColorStyles.appTextColor),
+                              onTap: () {
+                                setState(() {
+                                  _showIcons = !_showIcons;
+                                  imageName = imageName == 'zikir.jpg' ? 'meshed.jpeg' : 'zikir.jpg';
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Visibility(
+                    visible: !_showIcons,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: InkWell(
+                        child: Icon(Icons.settings,
+                            size: 32,
+                            color:
+                                imageName == 'zikir.jpg' ? ColorStyles.appBackGroundColor : ColorStyles.appTextColor),
+                        onTap: () {
+                          setState(() {
+                            _showIcons = !_showIcons;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
@@ -140,6 +180,7 @@ class _ZikirScreenState extends State<ZikirScreen> {
   _resetCounter() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
+      _showIcons = !_showIcons;
       counter = 0;
     });
     prefs.setInt('counter', counter);
